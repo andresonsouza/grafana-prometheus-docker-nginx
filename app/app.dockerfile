04578c00c900e53e1f510eacf8a44cb8e22906dd
Andreson Souza <andresonjazz@gmail.com>
2020-07-22 14:45:35 -0300

    Pegue uma metrica atraves do nginx

--- app/app.dockerfile
+++ app/app.dockerfile
@@ -3,6 +3,7 @@ MAINTAINER Andreson Souza
 COPY . /var/www
 WORKDIR /var/www
 RUN npm install
+RUN npm install --save express prom-client
 ENTRYPOINT ["npm", "start"]
 # Indica/informa qual porta o serviço(node) está expondo.
 EXPOSE 3030
--- app/index.js
+++ app/index.js
@@ -1,12 +1,27 @@
 const express = require('express')
+let prom = require('prom-client')
+const register = prom.register;
 let app = express();
 
 app.use(express.static("."));
 
-app.get("/", (req,res)=>{
-    res.sendFile(__dirname + '/index.html')
-})
+const counter = new prom.Counter({
+    name: 'status_request_total',
+    help: 'Contador de requests',
+    labelNames: ['statusCode']
+});
 
-app.listen("3030", ()=>{
+app.get("/", (req, res) => {
+    counter.labels('200').inc();
+
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/metrics', function (req, res) {
+    res.set('Content-Type', register.contentType);
+    res.end(register.metrics());
+});
+
+app.listen("3030", () => {
     console.log("Server is listening on port 3030")
-})
+});
